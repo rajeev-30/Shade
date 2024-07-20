b926786c5afb1687349233b02edcf61e543153fc
Rajeev Kushwaha <rajeevkushwaha1812@gmail.com>
2024-07-20 20:54:25 +0530

    Delete notification route added

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,6 +1,7 @@
 import { Notification } from "../models/notification.model.js";
 import { User } from "../models/user.model.js";
 
+//Get notifications
 export const getNotifications = async(req, res) =>{
     try {
         const id = req.userId;
@@ -26,6 +27,67 @@ export const getNotifications = async(req, res) =>{
         })
     } catch (error) {
         console.log("getNotifications error: " + error.message)
+        
+        return res.status(500).json({
+            message: "Internal server error",
+            success:false
+        })
+    }
+}
+
+//Delete notifications
+export const deleteNotifications = async (req, res) => {
+    try {
+        const id = req.userId;
+
+        const user = await User.findById(id);
+
+        if(!user){
+            return res.status(404).json({
+                message: "User not found",
+                success:false
+            })
+        }
+
+        await Notification.deleteMany({to: user._id});
+
+        return res.status(200).json({
+            message:"Deleted all notifications",
+            success:true,
+        })
+
+    } catch (error) {
+        console.log("deleteNotifications error: " + error.message)
+        
+        return res.status(500).json({
+            message: "Internal server error",
+            success:false
+        })
+    }
+}
+
+//Delete notification
+export const deleteNotification = async (req, res) => {
+    try {
+        const {id} = req.params
+        const notification = await Notification.findById(id);
+
+        if(!notification){
+            return res.status(404).json({
+                message: "Notification not found",
+                success:false
+            })
+        }
+
+        await Notification.deleteMany(id);
+
+        return res.status(200).json({
+            message:"Notification Deleted",
+            success:true,
+        })
+
+    } catch (error) {
+        console.log("deleteNotifications error: " + error.message)
         
         return res.status(500).json({
             message: "Internal server error",
--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import isAuthenticated from '../db/auth.js';
-import { getNotifications } from '../controllers/notification.controller.js';
+import { deleteNotification, deleteNotifications, getNotifications } from '../controllers/notification.controller.js';
 const router = express.Router();
 
 router.route('/notifications').get(isAuthenticated, getNotifications);
+router.route('/deletenotifications').delete(isAuthenticated, deleteNotifications)
+router.route('/deletenotification/:id').delete(isAuthenticated, deleteNotification)
 
 export default router;
