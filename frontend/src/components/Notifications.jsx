638c73ef6d641e2749f2f1a7ac1b75ea1cfee42c
Rajeev Kushwaha <rajeevkushwaha1812@gmail.com>
2024-07-22 16:24:17 +0530

    notification page added

---
+++ frontend/src/components/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notifications = () => {
+  return (
+    <div className='w-[46%]'>
+        <h1 className='text-2xl font-semibold p-4 border-b border-gray-800'>Notifications</h1>
+    </div>
+  )
+}
+
+export default Notifications
