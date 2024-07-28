3fe560bb247b5b0147d11739864210388edae444
Rajeev Kushwaha <rajeevkushwaha1812@gmail.com>
2024-07-29 00:57:57 +0530

    cors updated

--- backend/app.js
+++ backend/app.js
@@ -14,16 +14,27 @@ const app = express();
 const port = process.env.PORT || 4000
 
 //cors
+const allowedOrigins = ["http://localhost:5173", ""];
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials:true
-}
+    origin: (origin, callback) => {
+        // Allow requests with no origin (like mobile apps, curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true // Allow credentials
+};
+
+app.use(cors(corsOptions));
 
 app.use(cors(corsOptions));
 
 //middlewares
-app.use(express.urlencoded({extended:true}));
-app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 
 //api
@@ -31,9 +42,9 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/notification", notificationRoute);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server listening on port ${port}`);
-    
+
 })
 
 export default app;
