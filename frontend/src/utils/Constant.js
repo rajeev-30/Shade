36f5ae3b0f02d691d756bf6d3ac5bf867fd963ec
Rajeev Kushwaha <rajeevkushwaha1812@gmail.com>
2024-08-17 19:43:41 +0530

    endpoint changed

--- frontend/src/utils/Constant.js
+++ frontend/src/utils/Constant.js
@@ -1,8 +1,8 @@
-// export const USER_API_END_POINT = 'https://shade-rx.onrender.com/api/v1/user'
-// export const POST_API_END_POINT = 'https://shade-rx.onrender.com/api/v1/post'
-// export const NOTIFICATION_API_END_POINT = 'https://shade-rx.onrender.com/api/v1/notification'
+export const USER_API_END_POINT = 'https://shade-rx.onrender.com/api/v1/user'
+export const POST_API_END_POINT = 'https://shade-rx.onrender.com/api/v1/post'
+export const NOTIFICATION_API_END_POINT = 'https://shade-rx.onrender.com/api/v1/notification'
 
 
-export const USER_API_END_POINT = 'http://localhost:8000/api/v1/user'
-export const POST_API_END_POINT = 'http://localhost:8000/api/v1/post'
-export const NOTIFICATION_API_END_POINT = 'http://localhost:8000/api/v1/notification'
+// export const USER_API_END_POINT = 'http://localhost:8000/api/v1/user'
+// export const POST_API_END_POINT = 'http://localhost:8000/api/v1/post'
+// export const NOTIFICATION_API_END_POINT = 'http://localhost:8000/api/v1/notification'
